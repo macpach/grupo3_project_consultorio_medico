9b43ea7742a76cdca4670d4bae0102640c167070
macpach <juansebastianavilaarboleda@gmail.com>
2026-09-07 11:32:39 -0500

    creacion datos de consulta

--- CentroMedico/src/model/domain/consulta.java
+++ CentroMedico/src/model/domain/consulta.java
@@ -1,4 +1,52 @@
 package model.domain;
+
+import java.time.LocalDate;
+
 public class consulta {
+
+    private String motivo;
+    private String diagnostico;
+    private String tratamiento;
+    private LocalDate fecha;
+
+    public consulta(String motivo, String diagnostico, String tratamiento, LocalDate fecha) {
+        this.motivo = motivo;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        this.fecha = fecha;
+    }
+
+    // Getters y Setters
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 
 }
