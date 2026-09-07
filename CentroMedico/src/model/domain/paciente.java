23cf3bbe2ce047c4e20989ac3094e264e6928ea7
macpach <juansebastianavilaarboleda@gmail.com>
2026-09-07 11:29:25 -0500

    creacion de datos de paciente

--- CentroMedico/src/model/domain/paciente.java
+++ CentroMedico/src/model/domain/paciente.java
@@ -1,5 +1,35 @@
 package model.domain;
 public class paciente extends persona {
 
+    private int edad;
+    private String eps;
+
+    public paciente(String identificacion, String nombre, String telefono, int edad, String eps) {
+        super(identificacion, nombre, telefono);
+        this.edad = edad;
+        this.eps = eps;
+    }
+
+    @Override
+    public String rolEnConsulta() {
+        return "Rol: Paciente | EPS: " + eps + " | Edad: " + edad + " años";
+    }
+
+    // Getters y Setters
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
 
 }
